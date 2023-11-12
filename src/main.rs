79cc63e05f31e440356c32b23cb0e2901f92c9b4
Camilo <juguevarah@unal.edu.co>
2023-11-12 03:07:45 -0500

    Adding the first steps of the principal functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod index;
 use embedded_svc::http::Method;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 use esp_idf_hal::peripherals::Peripherals;
@@ -5,6 +6,7 @@ use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::http::server::{Configuration as HttpServerConfig, EspHttpServer};
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
+use index::index_html;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
@@ -40,7 +42,7 @@ fn main() {
     wifi.wait_netif_up().unwrap();
 
     // Get the local IP address
-    println!("Server IP address");
+    println!("Server IP address {{number}}",);
 
     // HTTP Configuration
     // Create HTTP Server Connection Handle
@@ -52,7 +54,7 @@ fn main() {
             // Retrieve html String
             let html = index_html();
             let uri = request.uri();
-            println!("{}", uri);
+            println!("{:?}", variables_format(uri));
             let mut response = request.into_ok_response().unwrap();
             // Return Requested Object (Index Page)
             response.write(html.as_bytes()).unwrap();
@@ -66,109 +68,11 @@ fn main() {
     }
 }
 
-fn index_html() -> String {
-    format!(
-        r#"
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-* {{
-    margin: 0;
-    padding: 0;
-}}
-
-.container {{
-    color: white;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #2d3250;
-}}
-
-.subcontainer {{
-    font-size: 1.5rem;
-    border-radius: 20px;
-    width: 50vw;
-    height: 60vh;
-    background-color: #424769;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-}}
-
-.formulario {{
-    width: 100%;
-    height: 40vh;
-    color: #f9b17a;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    font-size: 4rem;
-}}
-
-.options {{
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-}}
-
-p {{
-    text-align: center;
-}}
-
-input {{
-    text-align: center;
-    color: white;
-    background-color: transparent;
-    border: none;
-    border-bottom: 1px solid #676f9d;
-    outline: none;
-}}
-
-.send {{
-    width: 100px;
-    height: 30px;
-    overflow: hidden;
-    border-radius: 5px;
-    border: 1px solid black;
-    color: black;
-    background-color: #f9b17a;
-}}
-    </style>
-</head>
-<body>
-	<div class="container">
-		<div class="subcontainer">
-			<div class="title">
-				<h1>Pong-Master</h1>
-			</div>
-			<form class="formulario">
-				<div class="options">
-					<div class="velocity">
-						<p>V</p>
-						<input type="text" name="VelocidadValue">
-					</div>
-					<div class="angle">
-						<p>θ</p>
-						<input type="text" name="angleValue">
-					</div>
-				</div>
-				<input type="submit" value="Enviar" class="send">
-			</form>
-		</div>
-	</div>
-</body>
-
-
-</html>
-        "#
-    )
+fn variables_format(url: &str) -> Vec<u64> {
+    let variables: Vec<u64> = url
+        .split("&")
+        .flat_map(|x| x.split("=").nth(1))
+        .filter_map(|num| num.parse().ok())
+        .collect();
+    variables
 }
